8c3ad4d2d239e9060a85b1594f9b99b3653d15c7
velagalaAbhinav <62015508+velagalaAbhinav@users.noreply.github.com>
2021-06-09 12:04:02 +0530

    Add files via upload

---
+++ guessingGame.py
@@ -0,0 +1,14 @@
+import random
+ number = random(1,2,3,4,5,6,7,8,9)
+count = 5
+while(count>=0):
+    print(count)
+    count = count - 1
+if(guess == too high):
+    print("try Again u went too high")
+if(guess == very near):
+    print("u were very close")
+if(guess == number):
+    print("CROGRAGULATIONS ON UR NUMBER GUESSING")        
+if not chances<5:
+    print("u lost try again later",number)
